a0cf4d0d499aa6f79daeec331964e26c60f6a23e
Mitchell Dixon <mitchelldixon35@gmail.com>
2018-09-19 10:53:34 +0100

    Grid + anims

---
+++ Gun Toting Robots/Assets/Scripts/UI_Parts.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Parts : MonoBehaviour {
+
+    public GameObject PrefabToSpawn;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Mouse0)) // Left mouse click
+        {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 MousePos = new Vector2(pos.x, pos.y);
+
+            var Click = Physics2D.OverlapPoint(MousePos);
+
+            if (Click && Click.transform == transform) // If clicked then show game object
+            {
+                Instantiate(PrefabToSpawn, new Vector3(MousePos.x, MousePos.y, 0), Quaternion.identity);
+            }
+        }
+    }
+
+}
