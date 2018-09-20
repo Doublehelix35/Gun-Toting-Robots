300e9e2a46d377409d5a2a8c4443d11b15c9c207
Mitchell Dixon <mitchelldixon35@gmail.com>
2018-09-20 23:57:49 +0100

    Music Imported + Splash screen

---
+++ Gun Toting Robots/Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour {
+
+    float TimeStart;
+    public float TimeToWait = 1f;
+
+    void Start()
+    {
+        TimeStart = Time.time;
+    }
+
+    void Update ()
+    {
+        if (TimeStart + TimeToWait < Time.time)
+        {
+            // Load main menu
+            SceneManager.LoadScene("MainMenu");
+        }
+	}
+}
