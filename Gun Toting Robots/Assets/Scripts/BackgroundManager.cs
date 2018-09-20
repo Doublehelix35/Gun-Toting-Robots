5c3c4a6738eb771214d32275be3c7590101035bd
Mitchell Dixon <mitchelldixon35@gmail.com>
2018-09-20 10:01:40 +0100

    Background manager set up

---
+++ Gun Toting Robots/Assets/Scripts/BackgroundManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundManager : MonoBehaviour {
+
+    [SerializeField] BackgroundScrolling[] Backgrounds;
+
+    Vector2 LastFramePos;
+
+    // Use this for initialization
+    void Start()
+    {
+        LastFramePos = new Vector2(transform.position.x, transform.position.y); // Starting position
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 CurPos = new Vector2(transform.position.x, transform.position.y); // Current position
+
+        if (LastFramePos == CurPos) // if havent moved, exit
+        {
+            return;
+        }
+
+        // Calc pos
+        Vector2 pos;
+        pos = LastFramePos - CurPos;
+        pos.x = pos.x - (int)pos.x;
+        pos.y = pos.y - (int)pos.y;
+
+        foreach (BackgroundScrolling s in Backgrounds)
+        {
+            s.MoveTexture(pos);
+        }
+
+        // Update last frame pos
+        LastFramePos = CurPos;
+    }
+}
---
+++ Gun Toting Robots/Assets/Scripts/BackgroundScrolling.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScrolling : MonoBehaviour {
+
+    [Range(0, 1)] public float XSpeed = 1; // Speed to move on the x axis
+    [Range(0, 1)] public float YSpeed = 1; // Speed to move on the y axis
+
+    Vector2 offset = Vector2.zero;
+    Material mat; // Material to move
+
+    void Awake()
+    {
+        // Get material on gameobject
+        mat = GetComponent<MeshRenderer>().material;
+    }
+
+    public void MoveTexture(Vector2 pos)
+    {
+        // Calculate offset
+        offset.x += pos.x * XSpeed;
+        offset.y += pos.y * YSpeed;
+
+        if (offset.x > 1f)
+        {
+            offset.x -= 1f;
+        }
+        else if (offset.x < -1f)
+        {
+            offset.x += 1f;
+        }
+        if (offset.y > 1f)
+        {
+            offset.y -= 1f;
+        }
+        else if (offset.y < -1f)
+        {
+            offset.y += 1f;
+        }
+
+        // Set mat offset to new offset
+        mat.mainTextureOffset = offset;
+    }
+}
