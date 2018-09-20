99558437583ac44d9314575eac78c6e40af504de
Mitchell Dixon <mitchelldixon35@gmail.com>
2018-09-20 17:03:53 +0100

    Enemies started

---
+++ Gun Toting Robots/Assets/Scripts/Enemy.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public GameObject BulletPrefab;
+
+    // Stats
+    public float Health = 5f;
+    float CurrentHealth = 5f;
+    public float Attack = 1f;
+    public float Speed = 1f;
+    float BaseSpeed = 100f;
+    public float ShootCooldown;
+    public float BulletSpeed;
+
+    float LastShootTime;
+
+    GameObject PlayerRef;
+
+	void Start ()
+    {
+        // Modify stats with gausian randomness
+        GenerateStats();
+
+        // Init Values
+        CurrentHealth = Health;
+        LastShootTime = Time.time;
+        PlayerRef = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void GenerateStats()
+    {
+        Health = GausianModifier(Health);
+        Attack = GausianModifier(Attack);
+        Speed = GausianModifier(Speed);
+        ShootCooldown = GausianModifier(ShootCooldown);
+        BulletSpeed = GausianModifier(BulletSpeed);
+    }
+
+    float GausianModifier(float ValueToModify)
+    {
+        float NewValue = 0f;
+        float randTotal = 0f;
+
+        // 10 random numbers from a range of 0 to 2
+        for(int i = 0; i < 10; i++)
+        {
+            float rand = Random.Range(0f, 2f);
+            randTotal += rand;
+        }
+
+        randTotal = randTotal / 10;
+
+        NewValue = ValueToModify * randTotal;
+
+        return NewValue;
+    }
+}
